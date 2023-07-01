9d213507b61c8a339f87a61be4eb84647abc7490
giorgiomanca97 <giorgio.manca.97@gmail.com>
2023-07-01 11:41:53 +0200

    feat: add lti dynamic system

---
+++ src/dynamic_systems/dynamic_systems_control/include/dynamic_systems_control/lti.hpp
@@ -0,0 +1,72 @@
+/**
+ * Dynamic systems library base class.
+ *
+ * Giorgio Manca <giorgio.manca.97@gmail.com>
+ * Intelligent Systems Lab <isl.torvergata@gmail.com>
+ *
+ * June 29, 2023
+ */
+
+#ifndef DYNAMIC_SYSTEMS_CONTROL__LTI_HPP_
+#define DYNAMIC_SYSTEMS_CONTROL__LTI_HPP_
+
+#include "visibility_control.h"
+
+#include <dynamic_systems_base/dynamic_system.hpp>
+
+namespace DynamicSystems
+{
+  namespace Control 
+  {
+    struct DYNAMIC_SYSTEMS_CONTROL_PUBLIC LTIInitParams : public InitParams {
+      ~LTIInitParams() override;
+      std::unique_ptr<InitParams> clone() const override;
+      void copy(const InitParams &other) override;
+      
+      MatrixXd matrixA;
+      MatrixXd matrixB;
+      MatrixXd matrixC;
+      MatrixXd matrixD;
+    };
+
+    struct DYNAMIC_SYSTEMS_CONTROL_PUBLIC LTISetupParams : public SetupParams {
+      ~LTISetupParams() override;
+      std::unique_ptr<SetupParams> clone() const override;
+      void copy(const SetupParams &other) override;
+    };
+
+    struct DYNAMIC_SYSTEMS_CONTROL_PUBLIC LTIState : public State {
+      ~LTIState() override;
+      std::unique_ptr<State> clone() const override;
+      void copy(const State &other) override;
+
+      MatrixXd value;
+    };
+    
+    class DYNAMIC_SYSTEMS_CONTROL_PUBLIC LTISystem : public System {
+      protected:
+        void DYNAMIC_SYSTEMS_CONTROL_LOCAL init_parse(const InitParams& initParams) override;
+        void DYNAMIC_SYSTEMS_CONTROL_LOCAL setup_parse(const SetupParams& setupParams) override;
+        void DYNAMIC_SYSTEMS_CONTROL_LOCAL setup_default() override;
+        void DYNAMIC_SYSTEMS_CONTROL_LOCAL deinit() override;
+        void DYNAMIC_SYSTEMS_CONTROL_LOCAL state_validator(State &state) override;
+        void DYNAMIC_SYSTEMS_CONTROL_LOCAL input_validator(const State &state, MatrixXd &input) override;
+        void DYNAMIC_SYSTEMS_CONTROL_LOCAL dynamic_map(const State &state, const MatrixXd &input, State &next) override;
+        void DYNAMIC_SYSTEMS_CONTROL_LOCAL output_map(const State &state, const MatrixXd &input, MatrixXd& output) override;
+
+      private:
+        /* init members */
+        unsigned int n_;
+        unsigned int m_;
+        unsigned int q_;
+        MatrixXd A_;
+        MatrixXd B_;
+        MatrixXd C_;
+        MatrixXd D_;
+
+        /* setup members */
+    };
+  }
+}
+
+#endif // DYNAMIC_SYSTEMS_CONTROL__LTI_HPP_
---
+++ src/dynamic_systems/dynamic_systems_control/src/dynamic_systems_control/lti.cpp
@@ -0,0 +1,130 @@
+/**
+ * Dynamic systems library base class.
+ *
+ * Giorgio Manca <giorgio.manca.97@gmail.com>
+ * Intelligent Systems Lab <isl.torvergata@gmail.com>
+ *
+ * June 29, 2023
+ */
+
+#include <dynamic_systems_control/lti.hpp>
+
+namespace DynamicSystems
+{
+  namespace Control 
+  {
+    /* InitParams */
+
+    LTIInitParams::~LTIInitParams() {}
+
+    std::unique_ptr<InitParams> LTIInitParams::clone() const {
+      std::unique_ptr<LTIInitParams> res = std::make_unique<LTIInitParams>();
+      res->copy(*this);
+      return res;
+    }
+
+    void LTIInitParams::copy(const InitParams &other) {
+      InitParams::copy(other);
+      auto casted = dynamic_cast<const LTIInitParams&>(other);
+      this->matrixA = casted.matrixA;
+      this->matrixB = casted.matrixB;
+      this->matrixC = casted.matrixC;
+      this->matrixD = casted.matrixD;
+    }
+
+
+    /* SetupParams */
+
+    LTISetupParams::~LTISetupParams() {}
+
+    std::unique_ptr<SetupParams> LTISetupParams::clone() const {
+      std::unique_ptr<LTISetupParams> res = std::make_unique<LTISetupParams>();
+      res->copy(*this);
+      return res;
+    }
+
+    void LTISetupParams::copy(const SetupParams &other) {
+      SetupParams::copy(other);
+    }
+
+
+    /* State */
+
+    LTIState::~LTIState() {}
+
+    std::unique_ptr<State> LTIState::clone() const {
+      std::unique_ptr<LTIState> res = std::make_unique<LTIState>();
+      res->copy(*this);
+      return res;
+    }
+
+    void LTIState::copy(const State &other) {
+      State::copy(other);
+      auto casted = dynamic_cast<const LTIState&>(other);
+      this->value = casted.value;
+    }
+    
+
+    /* System */
+
+    void LTISystem::init_parse(const InitParams& initParams) {
+      auto casted = dynamic_cast<const LTIInitParams&>(initParams);
+
+      if(casted.matrixA.rows() != casted.matrixA.cols() ||
+         casted.matrixB.rows() != casted.matrixA.rows() ||
+         casted.matrixC.cols() != casted.matrixA.cols() ||
+         casted.matrixD.cols() != casted.matrixB.cols() ||
+         casted.matrixD.rows() != casted.matrixC.rows()) 
+      {
+        throw std::invalid_argument("Invalid matrices size for lti system.");
+      }
+
+      n_ = casted.matrixA.rows();
+      m_ = casted.matrixB.cols();
+      q_ = casted.matrixC.rows();
+      A_ = casted.matrixA;
+      B_ = casted.matrixB;
+      C_ = casted.matrixC;
+      D_ = casted.matrixD;
+
+      std::shared_ptr<LTIState> state = std::make_shared<LTIState>();
+      state->value = MatrixXd(n_, 1);
+      reset(state);
+      input(MatrixXd(m_, 1));
+      update();
+    }
+
+    void LTISystem::setup_parse(const SetupParams& setupParams) {
+      UNUSED(setupParams);
+    }
+
+    void LTISystem::setup_default() {}
+
+    void LTISystem::deinit(){}
+
+    void LTISystem::state_validator(State &state) {
+      const LTIState &state_casted = static_cast<const LTIState&>(state);
+      if(state_casted.value.rows() != n_ || state_casted.value.cols() != 1) {
+        throw std::invalid_argument("Invalid state size.");
+      }
+    }
+
+    void LTISystem::input_validator(const State &state, MatrixXd &input) {
+      UNUSED(state);
+      if(input.rows() != m_ || input.cols() != 1) {
+        throw std::invalid_argument("Invalid input size.");
+      }
+    }
+
+    void LTISystem::dynamic_map(const State &state, const MatrixXd &input, State &next) {
+      const LTIState &state_casted = static_cast<const LTIState&>(state);
+      LTIState &next_casted = static_cast<LTIState&>(next);
+      next_casted.value = A_ * state_casted.value + B_ * input;
+    }
+
+    void LTISystem::output_map(const State &state, const MatrixXd &input, MatrixXd& output) {
+      const LTIState &state_casted = static_cast<const LTIState&>(state);
+      output = C_ * state_casted.value + D_ * input;
+    }
+  }
+} 
